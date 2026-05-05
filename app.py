595d5318f759f4af23fcb478dea88ae6f8921932
Abdus samad <125046921+abdus-samad123@users.noreply.github.com>
2026-05-05 15:57:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if not st.session_state.logged_in:
     </style>
     """, unsafe_allow_html=True)
 
-    st.title("🌾 PragyanAI Login Portal")
+    st.title("🌾 Farmers  Login Portal")
 
     tab1, tab2, tab3 = st.tabs(["🔐 Login", "📝 Register", "🔁 Forgot Password"])
 
